eef26268bf88126634e3a7eebd7004dbe2eb1d40
Lukasz Samulnik <l.Samulnik@hypermedia.pl>
2017-06-05 12:19:53 +0200

    linting second component

---
+++ at_app/src/components/App.jsx
@@ -0,0 +1,22 @@
+import React, { PropTypes } from 'react';
+import Main from './Main';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <Main dataToRender={this.props.children} />
+    );
+  }
+}
+
+App.propTypes = {
+  children: PropTypes.string.isRequired,
+  //jesli to nie string to prosze o poprawienie, nie wiem jaki jest zamysl
+};
+
+export default App;
